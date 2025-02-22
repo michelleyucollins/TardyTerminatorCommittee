97c6b2534cb3f365a6c6886346baaa341aed14c3
Cindy <cindyjy.liu@mail.utoronto.ca>
2025-02-22 14:51:57 -0500

    Updated stop_name_processing.py to support subway data

--- preliminary/stop_name_processing.py
+++ preliminary/stop_name_processing.py
@@ -3,7 +3,8 @@ Please refer to the README of the preliminary folder for any information.
 
 Run stop_name_processing.py with the following command in your terminal. You should be in the home directory. 
 ```
-python -m preliminary.stop_name_processing  --file_path <json file> 
+python -m preliminary.stop_name_processing  --type <LLM or fuzzy>
+                                            --file_path <json file> 
                                             --stop_key <key of stop names> 
                                             --stop_path <txt csv file with stops>
                                             --stop_name <name of column with stop names>
@@ -21,9 +22,11 @@ import argparse
 from google import genai
 from personalconstants import GEMINI_API_KEY
 import time
+from fuzzywuzzy import process
 
 # parse the arguments
 parser = argparse.ArgumentParser()
+parser.add_argument('--type', default='fuzzy')
 parser.add_argument('--file_path', default='data/delays/bus-delay-data.json')
 parser.add_argument('--stop_key', default='Location')
 parser.add_argument('--stop_path', default='data/TTC Routes and Schedules Data/stops.txt')
@@ -32,9 +35,99 @@ parser.add_argument('--stop_code', default='stop_code')
 parser.add_argument('--output_name', default='preliminary/results.txt')
 parser.add_argument('--start', default=0)
 
-
-
 args = parser.parse_args()
+
+def gemini_matching(unprocessed_stops_df, stop_data, args):
+    # Create Prompt
+    standard_prompt = "You will be given 2 sets of information: \n\
+            1) CSVs containing stop data, where the first column is the stop id, and the second column is its name;\n\
+            2) A list of containing 10 names, which may be misspelled, concatenated, shortened, or anything in between, describing the stop names mentioned in the previous CSV.\n\
+            Your response should be a CSV file with 10 rows where the first column is the corresponding value from the list (2),\
+                the second column is the correct name,\
+                and the third column is the stop code.\
+                If you do not know what the correct name is, put NONE in the second and third column.\n\
+            Ensure that you have checked all possible stops in the stop data list. DO NOT add additional annotations.\n"
+
+    for i in range(int(args.start),len(unprocessed_stops_df)-10,10):
+
+        curr_unprocessed_stops_csv = unprocessed_stops_df[i:i+11].to_csv(index = False)
+
+        prompt = standard_prompt + "Information set 1:\n" + stop_data + "\n\nInformation set 2:\n" + curr_unprocessed_stops_csv
+        
+        try: 
+            # Call Gemini
+            client = genai.Client(api_key=GEMINI_API_KEY)
+            response = client.models.generate_content(
+                model="gemini-1.5-flash", contents=prompt
+            )
+
+            with open(args.output_name, "a") as file:
+                file.write(response.text)
+
+            count = 0
+        except:
+            time.sleep(30) # try to wait until API calls are refreshed
+            count+=1
+            if count > 10:
+                break
+
+    # If it forceably stops for any reason
+    print(f'Processing stopped at index {i}. Please continue with additional argument --start <i>.')
+
+def fuzzy_matching(unprocessed_stops_df, stop_data, args):
+    stop_name_list = list(stop_data.iloc(axis=1)[1])
+    bad_stop_data = list(unprocessed_stops_df.iloc(axis = 1)[0])
+
+    # Make header
+    prediction = ['bad_stop', 'confidence_1',  'stop_id_1', 'stop_name_1', 'confidence_2', 'stop_id_2', 'stop_name_2']
+    try:
+        with open(args.output_name, "w") as file:
+            wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+            wr.writerow(prediction)
+    except:
+        print(f'Writing to file failed for stop {bad_stop}')
+
+
+    for i in range(len(bad_stop_data)):
+        bad_stop = bad_stop_data[i]
+        try:
+            if bad_stop == "stc":
+                bad_stop = "Scarborough Town Centre"
+
+            if "stn" in bad_stop:
+                bad_stop = bad_stop[:bad_stop.index("stn")] + "station" + bad_stop[bad_stop.index("stn")+3:]
+
+            if "york u " in bad_stop:
+                bad_stop = bad_stop[:bad_stop.index("york u")] + "york university" + bad_stop[bad_stop.index("york u")+6:]
+            # Get the top 2 most correlated items
+            ans = process.extract(bad_stop, stop_name_list, limit=2)
+
+            # Extract the stop information
+            stop_1 = list(stop_data.iloc[stop_name_list.index(ans[0][0])])
+            stop_2 = list(stop_data.iloc[stop_name_list.index(ans[1][0])])
+
+
+            # We want to have the stop id, name and also confidence
+            # Confidence is necessary to do manual fixes after 
+            prediction = [bad_stop_data[i], ans[0][1], stop_1[0], stop_1[1],  ans[1][1], stop_2[0], stop_2[1]]
+
+            try:
+                with open(args.output_name, "a") as file:
+                    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+                    wr.writerow(prediction)
+            except:
+                print(f'Writing to file failed for stop {bad_stop_data[i]}')
+        except:
+            print(f"Process failed for {bad_stop_data[i]} at index {i}")
+            prediction = [bad_stop_data[i], "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"]
+            try:
+                with open(args.output_name, "a") as file:
+                    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+                    wr.writerow(prediction)
+            except:
+                print(f'Writing to file failed for stop {bad_stop_data[i]}')
+            continue
+
 
 # Load the data
 with open(args.file_path, "r") as file:
@@ -44,51 +137,14 @@ unprocessed_stops_df = pd.DataFrame(data[args.stop_key])
 
 stops_df = pd.read_csv(args.stop_path)
 
-stop_data = stops_df[[args.stop_code, args.stop_name]]
-stop_data = stop_data.to_csv(index=False)
+stop_data_df = stops_df[[args.stop_code, args.stop_name]]
+stop_data = stop_data_df.to_csv(index=False)
 unprocessed_stops = unprocessed_stops_df.to_csv(index=False)
 
-# Create Prompt
-standard_prompt = "You will be given 2 sets of information: \n\
-        1) CSVs containing stop data, where the first column is the stop id, and the second column is its name;\n\
-        2) A list of containing 10 names, which may be misspelled, concatenated, shortened, or anything in between, describing the stop names mentioned in the previous CSV.\n\
-        Your response should be a CSV file with 10 rows where the first column is the corresponding value from the list (2),\
-              the second column is the correct name,\
-              and the third column is the stop code.\
-              If you do not know what the correct name is, put NONE in the second and third column.\n\
-        Ensure that you have checked all possible stops in the stop data list. DO NOT add additional annotations.\n"
-
-full_response = ""
-
-for i in range(args.start,len(unprocessed_stops)-10,10):
-
-    curr_unprocessed_stops_csv = unprocessed_stops_df[i:i+11].to_csv(index = False)
-
-    prompt = standard_prompt + "Information set 1:\n" + stop_data + "\n\nInformation set 2:\n" + curr_unprocessed_stops_csv
-    
-    try: 
-        # Call Gemini
-        client = genai.Client(api_key=GEMINI_API_KEY)
-        response = client.models.generate_content(
-            model="gemini-1.5-flash", contents=prompt
-        )
-        print(response.text)
-        full_response = full_response+"\n\n"+response.text
-
-        with open(args.output_name, "a") as file:
-            file.write(response.text)
-
-        count = 0
-    except:
-        time.sleep(30) # try to wait until API calls are refreshed
-        count+=1
-        if count > 5:
-            break
-
-# Precautionary file
-with open("results_full.txt", "w") as file:
-    file.write(full_response) 
-
-print(f'Processing stopped at index {i}. Please continue with additional argument --start <i>.')
+#perform matching and export
+if args.type == "gemini":
+    gemini_matching(unprocessed_stops_df, stop_data, args)
+else:
+    fuzzy_matching(unprocessed_stops_df, stop_data_df, args)
 
 # Note to self: we can also try to use fuzzywuzzy for fuzze string matching?
